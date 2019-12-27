c48734c6c5c7c6c9083af2b0118fca43495cb7e3
zaina826 <zainschaban@gmail.com>
2019-12-27 12:00:33 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import './main.css';
+import logo from './/logo.png'
+export default class MainPage extends Component{
+  render(){
+   return(
+    <div>
+       <img src={logo} width="1000"/>
+    </div>
+   );
+
+}}
