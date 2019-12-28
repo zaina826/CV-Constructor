e80fd7b72823085ecc8bbe2a3dd452772d9fe59b
sobhiashraf912 <sobhi.asraf912@gmail.com>
2019-12-28 11:12:18 +0200

    Added home page and styles

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,20 @@
-import React,{Component} from 'react';
-import logo from '../logo.png'
-import '../Main.css'
-export default class Home extends Component{
-    render(){
-        return(
-            <div>
-            <img src={logo} width="130" height="100" alt="logo" class="Logo"/>
-            </div>
-    
-    )
-    }
+import React, { Component } from "react";
+import logo from "../logo.png";
+import "./main.css";
+import { BrowserRouter, Route, Switch, Link, Redirect } from "react-router-dom";
+export default class Home extends Component {
+     render() {
+          return (
+               <div className="App">
+                    <header className="App-header">
+                         <img src={logo} className="App-logo" alt="logo" />
+
+                         <h1>CV Creator</h1>
+                         <Link className="button_a" to="/page1">
+                              Start proccess!
+                         </Link>
+                    </header>
+               </div>
+          );
+     }
 }
