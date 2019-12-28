081144a972c82e563ffefdd57bac75405a416cc0
zaina826 <zainschaban@gmail.com>
2019-12-28 11:57:02 +0200

    Added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import {NavLink} from 'react-router-dom'
+export default class Navbar extends Component{
+    render(){
+        return(
+          <div>
+              <NavLink to ='/'>Home</NavLink>
+              <NavLink to ='/aboutus'>Aboutus</NavLink>
+              <NavLink to ='/contactus'>Contact</NavLink>
+          </div>
+        );
+    }
+}
