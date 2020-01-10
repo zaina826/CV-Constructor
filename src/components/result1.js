b5648faba6bee58bf2cdba6e50e4348c4619c8f7
zaina826 <zainschaban@gmail.com>
2020-01-10 15:51:05 +0200

    first

--- src/components/result1.js
+++ src/components/result1.js
@@ -23,20 +23,7 @@ class Res1 extends Component {
     return (
       <div className="App-header">
         <h3>Name:{this.state.name} </h3>
-
         <h3>Name:{this.state.address} </h3>
-        {/* <h3>Proffesional title: {match.params.PT}</h3>
-    <h3>Phone Number: {match.params.PN}</h3>
-    <h3>Gmail: {match.params.Gmail}</h3>
-    <h3>Address: {match.params.Address}</h3>
-    <h3>Linkdin: {match.params.In}</h3>
-    <h3>Major: {match.params.Major}</h3>
-    <h3>Name of university: {match.params.NOU}</h3>
-    <h3>Degrees: {match.params.Degree}</h3>
-    <h3>Skills: {match.params.Skills}</h3>
-    <h3>Profile: {match.params.Profile}</h3>
-    <h3>Work experiance: {match.params.WE}</h3>
-    <h3>Job title: {match.params.JT}</h3> */}
       </div>
     );
   }
